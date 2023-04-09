205f89bf863f251f86b221b703b3bf5de7882337
Chris <66511731+aioooi@users.noreply.github.com>
2023-04-09 19:02:12 +0200

    add startup script for CE instance

---
+++ infrastructure/compute-engine-startup-script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export INSTANCE_EXTERNAL_IP=`curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`
