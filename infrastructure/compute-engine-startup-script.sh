d11a6d9fbaad57d80470179df1b1ca897a765695
Chris <66511731+aioooi@users.noreply.github.com>
2023-04-09 21:14:19 +0200

    update startup-script

--- infrastructure/compute-engine-startup-script.sh
+++ infrastructure/compute-engine-startup-script.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-export INSTANCE_EXTERNAL_IP=`curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`
-
-echo "startup script"
+echo "export INSTANCE_EXTERNAL_IP=`curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`" >> /etc/profile
