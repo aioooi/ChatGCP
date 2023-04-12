5dc3bc480078ebfc263fbb41c1f4d698eb1fbf09
Chris <66511731+aioooi@users.noreply.github.com>
2023-04-12 22:30:32 +0200

    implement naive session handling in websockets

--- backend/websockets/setup.go
+++ backend/websockets/setup.go
@@ -1,37 +1,80 @@
 package websockets
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 
 	"github.com/olahol/melody"
 )
 
-// Prints relevant parts of an http.Request
-func printRequest(r *http.Request) {
-	// for k, v := range h.Header {
-	// 	fmt.Println(k)
-	// 	fmt.Println(v)
-	// }
-	fmt.Println(r.Header["Sec-Websocket-Key"])
+type MessageType string
+
+const (
+	// PROTOCOL_ACTION MessageType = "action"
+	ACTION_LOGIN MessageType = "action-login"
+	// PROTOCOL_EVENT  MessageType = "event"
+	EVENT_LOGIN  MessageType = "event-login"
+	EVENT_LOGOUT MessageType = "event-logout"
+	PAYLOAD      MessageType = "payload"
+)
+
+type Message struct {
+	Type    MessageType `json:"type"`
+	Payload []byte      `json:"payload"`
+}
+
+// Maps "Sec-Webscocket-Key"s to user names of "logged in" users (or "" for
+// connected sessions with undefined user name (== not "logged in")).
+var session = make(map[string]string)
+
+// First step of every melody handler:
+// print some log line and return Sec-Websocket-Key
+func initHandler(s *melody.Session, name string) string {
+	fmt.Println("\n\n" + name + "\n----")
+	k := s.Request.Header["Sec-Websocket-Key"][0]
+	fmt.Println(k)
+	return k
 }
 
 // Sets up the melody handlers
 func Setup(m *melody.Melody) {
 	m.HandleConnect(func(s *melody.Session) {
-		fmt.Println("\n\nHandleConnect\n------")
-		printRequest(s.Request)
+		k := initHandler(s, "handleConnect")
+		session[k] = ""
+		fmt.Println(session)
 	})
 
 	m.HandleDisconnect(func(s *melody.Session) {
-		fmt.Println("\n\nHandleDisconnect\n------")
-		printRequest(s.Request)
+		k := initHandler(s, "handleDisconnect")
+		delete(session, k)
+		fmt.Println(session)
+
+		m.Broadcast([]byte("{\"type\":" + EVENT_LOGOUT + "}"))
 	})
 
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		fmt.Println("\n\nHandleMessage\n------")
-		m.Broadcast(msg)
-		printRequest(s.Request)
+	m.HandleMessage(func(s *melody.Session, _msg []byte) {
+		k := initHandler(s, "handleMessage")
+
+		var msg Message
+		err := json.Unmarshal(_msg, &msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		switch msg.Type {
+		case ACTION_LOGIN:
+			// This is the only action so far, if more do another (outer, on
+			// PROTOCOL_ACTION, PROTOCOL_EVENT, PAYLOAD) switch/case and handle type
+			// of PROTOCOL_ACTION in inner switch/case, expecting action type such as
+			// ACTION_LOGIN as part of the payload.
+			session[k] = string(msg.Payload)
+			fmt.Println(session)
+
+			m.Broadcast([]byte("{\"type\":" + EVENT_LOGIN + "}"))
+
+		case PAYLOAD:
+			m.Broadcast(_msg)
+		}
 	})
 }
-
