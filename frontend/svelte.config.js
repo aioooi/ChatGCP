ef71ceb4d30d194b72c7119b5160ac938077e851
Chris <66511731+aioooi@users.noreply.github.com>
2023-08-25 11:36:21 +0200

    fix

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -13,15 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			// // using "index.html" as a "fallback" here as in
-			// // https://kit.svelte.dev/docs/single-page-apps turns it into an SPA:
-			// fallback: 'index.html',
-			precompress: true,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: dev ? '' : '/ChatGCP'
 		}
